911e7727f34b1f8e00a678c389849f21fafc3b34
Gourav Phulwaria <phulls@github.com>
2020-03-18 23:09:16 +0530

    Updates

---
+++ precache-manifest.603f7feb2cecd366a31f7f7f642bae3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d61406ec5f325ac8186b229ec63cb75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "920876260fb6ec93b811",
+    "url": "/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "4c6b0cba784b051f0934",
+    "url": "/static/css/main.2d51ff4f.chunk.css"
+  },
+  {
+    "revision": "920876260fb6ec93b811",
+    "url": "/static/js/2.d9752dae.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.d9752dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c6b0cba784b051f0934",
+    "url": "/static/js/main.f424a91c.chunk.js"
+  },
+  {
+    "revision": "484f37fd2f090a153baa",
+    "url": "/static/js/runtime-main.f324d9f5.js"
+  }
+]);
